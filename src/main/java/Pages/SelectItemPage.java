4c7a2a4ab005dc2678cdc44332e82066e054269c
kholoud Shaban <71041985+KholoudShabanAbdElRezaq@users.noreply.github.com>
2024-09-26 11:08:35 +0300

    SelectItemPage.java

--- src/main/java/Pages/SelectItemPage.java
+++ src/main/java/Pages/SelectItemPage.java
@@ -2,7 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+//
 public class SelectItemPage {
     private WebDriver webDriver;
     private By selectitem = By.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal']");
