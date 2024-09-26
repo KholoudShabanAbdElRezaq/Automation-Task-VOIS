de8381de6d457274513d6160965174828c4873c3
kholoud Shaban <71041985+KholoudShabanAbdElRezaq@users.noreply.github.com>
2024-09-26 11:08:56 +0300

    TodayDaily.java

--- src/main/java/Pages/TodayDaily.java
+++ src/main/java/Pages/TodayDaily.java
@@ -2,7 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+//
 public class TodayDaily {
     private WebDriver webDriver;
     private By TodayDaily = By.xpath("//a[@href='/gp/goldbox?ref_=nav_cs_gb']");
